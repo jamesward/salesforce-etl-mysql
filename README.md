c9f2e95ae9f5cb7094bf8588a8edeca5a4c5019a
James Ward <james@jamesward.org>
2016-08-02 15:17:27 -0600

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+Salesforce ETL MySQL
+--------------------
+
+This sample application shows a simple way to use a Node.js app along with Workflow on Salesforce to do Extract, Transform, and Load (ETL) from Salesforce to MySQL.
+
+### Run on Heroku
+1. Deploy the app: [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+1. Setup the contact table:
+
+        create table contact (id VARCHAR(18) PRIMARY KEY, name VARCHAR(128), email VARCHAR(128));
+
+
+### Run Locally
+
+1. Create a local MySQL database named `demo`
+1. Create a table:
+
+        create table contact (id VARCHAR(18) PRIMARY KEY, name VARCHAR(128), email VARCHAR(128));
+
+1. Install the Node.js dependencies:
+
+        npm install
+
+1. Run the local dev server:
+
+        npm run dev
+
+1. Start an [ngrok](https://ngrok.com/) tunnel:
+
+        ngrok http 5000
+
+
+### Setup a Salesforce Workflow & Outbound Message
+
+1. [Create a new Workflow](https://login.salesforce.com/01Q)
+1. Select the `Contact` object
+1. Give the rule a name
+1. Select `created, and every time it's edited`
+1. In the `Rule Critera` select `forumla evaluates to true` and enter `True` in the formula field
+1. In `Immediate Workflow Actions` select `New Outbound Message`
+1. Give the Outbound Message a name, enter the `Endpoint URL` for either your Heroku app (e.g. `https://foo.herokuapp.com/`) or your ngrok endpoint for local testing (e.g. `https://1234.ngrok.io/`)
+1. Select the `Email`, `FirstName`, and `LastName` fields
---
+++ server.js
@@ -0,0 +1,68 @@
+let app = require('express')();
+let xmlparser = require('express-xml-bodyparser')({explicitArray: false});
+let mysql = require('mysql');
+
+
+let tableName = 'contact';
+
+function transform(sobject) {
+  return {
+    'id': sobject['sf:id'],
+    'name': sobject['sf:firstname'] + ' ' + sobject['sf:lastname'],
+    'email': sobject['sf:email']
+  };
+}
+
+
+app.use(xmlparser);
+
+let connection = mysql.createConnection(process.env.DATABASE_URL || 'mysql://root@localhost/demo');
+
+connection.connect();
+
+function ack() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <notificationsResponse xmlns:ns2="urn:sobject.enterprise.soap.sforce.com" xmlns="http://soap.sforce.com/2005/09/outbound">
+          <Ack>true</Ack>
+        </notificationsResponse>
+      </soap:Body>
+    </soap:Envelope>`;
+}
+
+function nack(errorMessage) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <soapenv:Body>
+        <soapenv:Fault>
+          <faultcode>soap:Receiver</faultcode>
+          <faultstring>${errorMessage}</faultstring>
+        </soapenv:Fault>
+      </soapenv:Body>
+    </soapenv:Envelope>`;
+}
+
+app.post('/', function(req, res) {
+
+  try {
+    let sobject = req.body['soapenv:envelope']['soapenv:body']['notifications']['notification']['sobject'];
+    let data = transform(sobject);
+
+    connection.query(`INSERT INTO ${tableName} SET ? ON DUPLICATE KEY UPDATE ?`, [data, data], function(err) {
+      if (err != null) {
+        console.error('Database error', err.message);
+        res.status(500).send(nack(err.message));
+      }
+      else {
+        res.status(200).send(ack());
+      }
+    });
+  }
+  catch (err) {
+    console.error('Uncaught error', err);
+    res.status(500).send(nack(err.message));
+  }
+});
+
+app.listen(process.env.PORT || 5000);
